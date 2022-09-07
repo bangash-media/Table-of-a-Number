ac103b960f61eb515bf75bf1ca2be069dc8a43a2
Muhammad Waqar <111599744+bangash-media@users.noreply.github.com>
2022-09-07 14:26:17 +0500

    Add files via upload

---
+++ Lab2-T2-211232.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int number;
+	cout<<"\n\nEnter number for table: "; cin>>number;
+	cout<<"\n***********************************************\n\n";
+	cout<<"\t"<<number<<" * "<<1<<" = "<<number*1<<endl;
+	cout<<"\t"<<number<<" * "<<2<<" = "<<number*2<<endl;
+	cout<<"\t"<<number<<" * "<<3<<" = "<<number*3<<endl;
+	cout<<"\t"<<number<<" * "<<4<<" = "<<number*4<<endl;
+	cout<<"\t"<<number<<" * "<<5<<" = "<<number*5<<endl;
+	cout<<"\t"<<number<<" * "<<6<<" = "<<number*6<<endl;
+	cout<<"\t"<<number<<" * "<<7<<" = "<<number*7<<endl;
+	cout<<"\t"<<number<<" * "<<8<<" = "<<number*8<<endl;
+	cout<<"\t"<<number<<" * "<<9<<" = "<<number*9<<endl;
+	cout<<"\t"<<number<<" * "<<10<<" = "<<number*10<<endl;
+	cout<<"\n***********************************************";
+	return 0;
+}
